e12a66066f0966ef7ceb431f3b3ae49590e4c29c
IDGS-802-21000388 <21000388@alumnos.utleon.edu.mx>
2024-01-29 22:45:39 -0600

    JJMO - Macros - 29-Ene-24

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import forms 
 
 app = Flask(__name__)
 
@@ -6,9 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/alumnos')
+@app.route('/alumnos', methods=["GET","POST"])
 def alumnos():
-    return render_template('alumnos.html')
+    alum_form = forms.UserForm(request.form)
+    if request.method == "POST":
+        nom = alum_form.nombre.data
+        apa = alum_form.apaterno.data
+        correo = alum_form.email.data
+        print("Nombre: {}".format(nom))
+        print("apaterno: {}".format(apa))
+        print("correo: {}".format(correo))
+
+    return render_template('alumnos.html', form = alum_form, nom = nom, apa = apa, correo = correo)
 
 @app.route('/maestros')
 def maestros():
